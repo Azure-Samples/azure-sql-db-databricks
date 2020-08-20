8593fa80e4810616f5b95f51f1f328d6ab3acb57
Davide Mauri <damauri@microsoft.com>
2020-08-19 17:09:48 -0700

    sample 01 completed with both connectors

--- notebooks/01-load-into-single-table.scala
+++ notebooks/01-load-into-single-table.scala
@@ -4,7 +4,7 @@
 // MAGIC 
 // MAGIC In Azure SQL terminology an Heap is a table with no clustered index. In this samples we'll load data into a table that as no index (clustered or non-clustered) as is not partitioned. This is the simplest scenario possibile and allows parallel load of data.
 // MAGIC 
-// MAGIC Sample is using the new sql-spark-connector (https://github.com/microsoft/sql-spark-connector). To install manually import the .jar file (available in GitHub repo's releases) into the cluster.
+// MAGIC Sample is using both the new sql-spark-connector (https://github.com/microsoft/sql-spark-connector), and the previous one (https://github.com/Azure/azure-sqldb-spark). To install the _new connector_ manually import the .jar file (available in GitHub repo's releases) into the cluster. To install the previous one, just import the library right from Databricks portal using the "com.microsoft.azure:azure-sqldb-spark:1.0.2" coordinates.
 
 // COMMAND ----------
 
@@ -22,6 +22,8 @@ val server = dbutils.secrets.get(scope, "srv001").concat(".database.windows.net"
 val database = dbutils.secrets.get(scope, "db001");
 val user = dbutils.secrets.get(scope, "dbuser001");
 val password = dbutils.secrets.get(scope, "dbpwd001");
+val table = "dbo.LINEITEM_LOADTEST"
+
 
 // COMMAND ----------
 
@@ -70,50 +72,118 @@ li.printSchema
 // COMMAND ----------
 
 // MAGIC %md
+// MAGIC All columns are shown as nullable, even if they were originally set to NOT NULL, so we will need to keep this in mind later.
+
+// COMMAND ----------
+
+// MAGIC %md
 // MAGIC Make sure you create on your Azure SQL the following LINEITEM table:
 // MAGIC ```sql
 // MAGIC create table [dbo].[LINEITEM_LOADTEST]
 // MAGIC (
 // MAGIC 	[L_ORDERKEY] [int] not null,
 // MAGIC 	[L_PARTKEY] [int] not null,
-// MAGIC 	[L_SUPPKEY] [int] null,
+// MAGIC 	[L_SUPPKEY] [int] not null,
 // MAGIC 	[L_LINENUMBER] [int] not null,
-// MAGIC 	[L_QUANTITY] [decimal](15, 2) null,
-// MAGIC 	[L_EXTENDEDPRICE] [decimal](15, 2) null,
-// MAGIC 	[L_DISCOUNT] [decimal](15, 2) null,
-// MAGIC 	[L_TAX] [decimal](15, 2) null,
-// MAGIC 	[L_RETURNFLAG] [char](1) null,
-// MAGIC 	[L_LINESTATUS] [char](1) null,
-// MAGIC 	[L_SHIPDATE] [date] null,
-// MAGIC 	[L_COMMITDATE] [date] null,
-// MAGIC 	[L_RECEIPTDATE] [date] null,
-// MAGIC 	[L_SHIPINSTRUCT] [char](25) null,
-// MAGIC 	[L_SHIPMODE] [char](10) null,
-// MAGIC 	[L_COMMENT] [varchar](44) null,
+// MAGIC 	[L_QUANTITY] [decimal](15, 2) not null,
+// MAGIC 	[L_EXTENDEDPRICE] [decimal](15, 2) not null,
+// MAGIC 	[L_DISCOUNT] [decimal](15, 2) not null,
+// MAGIC 	[L_TAX] [decimal](15, 2) not null,
+// MAGIC 	[L_RETURNFLAG] [char](1) not null,
+// MAGIC 	[L_LINESTATUS] [char](1) not null,
+// MAGIC 	[L_SHIPDATE] [date] not null,
+// MAGIC 	[L_COMMITDATE] [date] not null,
+// MAGIC 	[L_RECEIPTDATE] [date] not null,
+// MAGIC 	[L_SHIPINSTRUCT] [char](25) not null,
+// MAGIC 	[L_SHIPMODE] [char](10) not null,
+// MAGIC 	[L_COMMENT] [varchar](44) not null,
 // MAGIC 	[L_PARTITION_KEY] [int] not null
-// MAGIC )
+// MAGIC ) 
 // MAGIC ```
 
 // COMMAND ----------
 
-// MAGIC %md
-// MAGIC Load the table using Bulk Copy API. As table is empty and there are no indexes and we suppose we will not have any other activity running on the table at the same time, we can enable the option `tableLock` so that data can be loaded in parallel, even if the table is *not* partitioned. By default Databricks will have execute one bulk load task for each CPU available in the worker nodes. 
+display(li.filter($"L_PARTITION_KEY" === 199202))
 
 // COMMAND ----------
 
-val table_name = "dbo.LINEITEM_TESTLOAD"
+// MAGIC %md
+// MAGIC ## Using the new connector
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC Schema needs to be defined explicitly as new connector is very sensitive to nullability, as per the following issue [Nullable column mismatch between Spark DataFrame & SQL Table Error](
+// MAGIC https://github.com/microsoft/sql-spark-connector/issues/5), so we need to explicity create the schema and apply it to the loaded data
+
+// COMMAND ----------
+
+import org.apache.spark.sql.types._
+
+val schema = StructType(
+    StructField("L_ORDERKEY", IntegerType, false) ::
+    StructField("L_PARTKEY", IntegerType, false) ::
+    StructField("L_SUPPKEY", IntegerType, false) ::  
+    StructField("L_LINENUMBER", IntegerType, false) ::
+    StructField("L_QUANTITY", DecimalType(15,2), false) ::
+    StructField("L_EXTENDEDPRICE", DecimalType(15,2), false) ::
+    StructField("L_DISCOUNT", DecimalType(15,2), false) ::
+    StructField("L_TAX", DecimalType(15,2), false) ::
+    StructField("L_RETURNFLAG", StringType, false) ::
+    StructField("L_LINESTATUS", StringType, false) ::
+    StructField("L_SHIPDATE", DateType, false) ::
+    StructField("L_COMMITDATE", DateType, false) ::
+    StructField("L_RECEIPTDATE", DateType, false) ::
+    StructField("L_SHIPINSTRUCT", StringType, false) ::  
+    StructField("L_SHIPMODE", StringType, false) ::  
+    StructField("L_COMMENT", StringType, false) ::  
+    StructField("L_PARTITION_KEY", IntegerType, false) ::  
+    Nil)
+    
+  val li2 = spark.createDataFrame(li.rdd, schema)
+
+// COMMAND ----------
+
 val url = s"jdbc:sqlserver://$server;databaseName=$database;"
 
-li.write 
+li2.write 
   .format("com.microsoft.sqlserver.jdbc.spark") 
-  .mode("overwrite") 
+  .mode("overwrite")   
   .option("truncate", "true") 
   .option("url", url) 
-  .option("dbtable", table_name) 
+  .option("dbtable", table) 
   .option("user", user) 
   .option("password", password) 
-  .option("applicationintent", "ReadWrite") 
   .option("reliabilityLevel", "BEST_EFFORT") 
   .option("tableLock", "true") 
   .option("batchsize", "100000") 
   .save()
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC # Using the old connector:
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC This connector is more permissive about schema so we can just use the schema coming from Parquet file
+
+// COMMAND ----------
+
+import com.microsoft.azure.sqldb.spark.bulkcopy.BulkCopyMetadata
+import com.microsoft.azure.sqldb.spark.config.Config
+import com.microsoft.azure.sqldb.spark.connect._
+
+val config = Config(Map(
+  "url" -> server,
+  "databaseName" -> database,
+  "dbTable" -> table,
+  "user" -> user,
+  "password" -> password,
+  "bulkCopyBatchSize" -> "100000",
+  "bulkCopyTableLock" -> "true",  
+  "bulkCopyTimeout" -> "600" //seconds  
+))
+
+li.bulkCopyToSqlDB(config)
